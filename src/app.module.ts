17285f642c4cdfd987b5412af52cb0f097719634
chvarkov <chvarkov.alexey@gmail.com>
2020-10-11 19:13:43 +0300

    Usage v1.0.13

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,17 @@
-import { BadRequestException, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { GoogleRecaptchaModule } from '@nestlab/google-recaptcha';
 import { Config } from './core/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path'
+import { recaptchaErrorHandler } from './helpers/recaptcha-error-handler';
 
 @Module({
     imports: [
         GoogleRecaptchaModule.forRoot({
             secretKey: Config.recaptchaSecret,
             response: req => req.headers.recaptcha,
-            onError: () => {
-                throw new BadRequestException('Invalid captcha');
-            },
+            onError: recaptchaErrorHandler,
         }),
         ServeStaticModule.forRoot({
             rootPath: join(__dirname, '..', 'client'),
