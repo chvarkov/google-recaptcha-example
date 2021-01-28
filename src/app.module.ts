4426c9ad44e0ba102e1cad4adbf92c1553fe8ee6
chvarkov <chvarkov.alexey@gmail.com>
2021-01-29 02:22:32 +0300

    Usage v1.2.2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path'
+import { join } from 'path';
 import { APP_MODULES } from './modules';
 import { GoogleRecaptchaModule } from '@nestlab/google-recaptcha';
 import { ConfigModule, ConfigService } from './config';
